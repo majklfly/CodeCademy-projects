978f644744058db9f21939e4e29094115baf5402
majklfly <majklfly@gmail.com>
2020-10-19 22:31:26 +0100

    added get all route

---
+++ pressPublishing/api/artists.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const sqlite3 = require("sqlite3");
+
+const db = new sqlite3.Database(
+  process.env.TEST_DATABASE || "./database.sqlite"
+);
+
+const artistsRouter = express.Router();
+
+artistsRouter.get("/", (req, res, next) => {
+  db.all(
+    "SELECT * FROM Artist WHERE is_currently_employed=1",
+    (error, rows) => {
+      if (error) {
+        next(error);
+      } else {
+        res.status(200).json({ artists: rows });
+      }
+    }
+  );
+});
+
+module.exports = artistsRouter;
